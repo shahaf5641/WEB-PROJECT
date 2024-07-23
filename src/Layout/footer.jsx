51764daa3774858ed516dd6f3ef5c5ecbe723360
shahaf5641 <shahaf564@gmail.com>
2024-07-23 16:10:07 +0300

    bbaba

--- src/Layout/footer.jsx
+++ src/Layout/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-        <h3 className='bg-green-500 p-4'>All rights reserved to BLABLABLABLALBLA</h3>
+        <h3 className='bg-slate-100 p-4 text-xl'>All rights reserved to BLABLABLABLALBLA</h3>
     </footer>
   )
 }
--- src/Layout/header.jsx
+++ src/Layout/header.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import NavLink from '../comps/navlink';
+import logolight from '../assets/logolight.png';
+import logodark from '../assets/logodark.png';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className="bg-slate-600 text-black p-4 text-lg nav">
       <div className="mx-auto items-center">
         <nav className="flex justify-between">
-          <NavLink to="/">MyLogo</NavLink>
+          <Link to="/">
+            <img src={logodark} alt="Logo" className="h-8 w-auto rounded-2xl" />
+          </Link>
           <NavLink to="/about">About</NavLink>
           <NavLink to="/contact">Contact</NavLink>
           <NavLink to="/dashboard">Dashboard</NavLink>
