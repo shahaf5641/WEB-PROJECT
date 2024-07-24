7b90e7b06228c44c00569acd118ea423bc5f2d1a
shahaf5641 <shahaf564@gmail.com>
2024-07-24 11:55:17 +0300

    fg

--- src/comps/VantaBackground.jsx
+++ src/comps/VantaBackground.jsx
@@ -14,8 +14,8 @@ const VantaBackground = () => {
       minWidth: 200.00,
       scale: 1.00,
       scaleMobile: 1.00,
-      color: 0x1fff,
-      backgroundColor: 0x1f1f61,
+      color: 0xf5f5f5,
+      backgroundColor: 0x8c8c8c,
       points: 16.00,
       maxDistance: 23.00,
       spacing: 19.00
