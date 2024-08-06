3c3535ede20ba21b9344ae59620ccb2fa7e08d8a
shahaf5641 <shahaf564@gmail.com>
2024-08-06 15:02:51 +0300

    דד

--- src/comps/home/CryptoCard.jsx
+++ src/comps/home/CryptoCard.jsx
@@ -44,9 +44,6 @@ export default function CryptoCard({ imgSrc, title, description, address }) {
                   strokeLinecap="round"
                 ></path>
               </svg>
-              <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
-                <div className="relative h-full w-10 bg-white/30"></div>
-              </div>
             </button>
           </div>
         </div>
