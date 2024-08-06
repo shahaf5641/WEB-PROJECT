be1e6b33312c9e023b66c544696f16d63044d87d
shahaf5641 <shahaf564@gmail.com>
2024-08-06 11:42:18 +0300

    dd

--- src/comps/contexts/DarkModeContext.jsx
+++ src/comps/contexts/DarkModeContext.jsx
@@ -11,7 +11,7 @@ export const DarkModeProvider = ({ children }) => {
 
   return (
     <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
-      <div style={{ maxHeight: '1800px'}}>
+      <div style={{ maxHeight: '1800px', overflowY: 'auto' }}>
         {children}
       </div>
     </DarkModeContext.Provider>
