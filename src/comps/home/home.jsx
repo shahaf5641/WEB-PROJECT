28c65a402bbed89b9ed24f3cc6481ed764e74b92
shahaf5641 <shahaf564@gmail.com>
2024-08-11 16:46:43 +0300

    asd

--- src/comps/home/home.jsx
+++ src/comps/home/home.jsx
@@ -5,7 +5,7 @@ import JoinSection from './JoinSection';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col">
       <div className="container mx-auto px-4 py-8 flex-grow">
         <HeroSection />
         <CryptoGrid />
